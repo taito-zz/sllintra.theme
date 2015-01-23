7bee13924cb82d62244dfba3754ba8985a1b8703
Taito Horiuchi <taito.horiuchi@gmail.com>
2015-01-23 16:09:20 +0200

    release 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sllintra.theme',
-    version='0.0.1',
+    version='0.0.2',
     description="SLL intra theme",
     long_description=open("README.rst").read(),
     classifiers=[
